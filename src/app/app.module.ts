be6b6aa5e1a4ea5d4346b5fa8620231d294e028b
yujung <cyj@tdcc.com.tw>
2024-05-07 20:00:51 +0800

    新增元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
