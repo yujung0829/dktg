0da08a3eaf8843c29cbca33be155eedc3ef7a1dd
yujung <yujung0829@gmail.com>
2024-05-15 13:31:34 +0800

    調整版面

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ArticlesComponent } from './articles/articles.component';
+import { FooterComponent } from './footer/footer.component';
+import { TagsComponent } from './tags/tags.component';
+import { NavItemsComponent } from './nav-items/nav-items.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ArticlesComponent
+    ArticlesComponent,
+    FooterComponent,
+    TagsComponent,
+    NavItemsComponent,
+    SidebarComponent,
+    TopBarComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
